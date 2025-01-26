1b9099a8c3051250eb4f9c056b3717174020055e
morguldir <morguldir@protonmail.com>
2025-01-26 02:48:35 +0100

    add stability

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-stable";
   };
   outputs = inputs@{ self, nixpkgs, ... }: {
     nixosConfigurations.strawberry = nixpkgs.lib.nixosSystem {
